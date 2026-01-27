6b72bdde2741855f0601cedc24a68dfc1b8ea872
Christopher G <135375609+AI-Ocko@users.noreply.github.com>
2026-01-27 14:04:02 -0800

    Created grid using flexbox as instructed

--- script.js
+++ script.js
@@ -2,4 +2,12 @@
 // all new grids will be generated within this div
 const parent = document.querySelector(".container");
 
+// set initial gridSize
+const gridSize = 16;
 
+// fill in the number of gridsquares
+for (let i = 0; i < gridSize * gridSize; i++) {
+  const child = document.createElement("div");
+  child.classList.add("gridSquare");
+  parent.appendChild(child);
+}
