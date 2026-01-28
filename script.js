dd3e0eaa946ff8c805090f31f7708f57b224a5b3
Christopher G <135375609+AI-Ocko@users.noreply.github.com>
2026-01-28 09:55:20 -0800

    Added button for user input to change grid size
    refactored grid generation, now located in function createGrid
    createGrid called after user input form button press

--- script.js
+++ script.js
@@ -1,17 +1,35 @@
+// logic for generating the drawing grid
+function createGrid(size) {
+  // clear the existing grid
+  parent.innerHTML = "";
+  // change the var in css sheet to whatever the input size is
+  parent.style.setProperty("--grid-size", size);
+
+  for (let i = 0; i < size * size; i++) {
+    const child = document.createElement("div");
+    child.classList.add("gridSquare");
+    parent.appendChild(child);
+
+    child.addEventListener("mouseover", () => {
+      child.style.backgroundColor = "black";
+    })
+  }
+
+}
+
 // select parent node as .container from html file
 // all new grids will be generated within this div
 const parent = document.querySelector(".container");
 
 // set initial gridSize
-const gridSize = 16;
+let gridSize = 16;
 
-for (let i = 0; i < gridSize * gridSize; i++) {
-  // fill in the number of gridsquares
-  const child = document.createElement("div");
-  child.classList.add("gridSquare");
-  parent.appendChild(child);
+createGrid(gridSize);
 
-  child.addEventListener("mouseover", () => {
-    child.style.backgroundColor = "black";
-  })
-}
+// option for user to change the grid size
+const button = document.querySelector("#reset");
+
+button.addEventListener("click", () => {
+  const inputSize = prompt("Enter a range from 16 to 100: ");
+  createGrid(inputSize)
+})
